7a300fa6611fcd5a31f735b3976b6ffe19d87d3a
Wrenna Robson <wrenna.robson@bristol.ac.uk>
2026-01-22 13:17:52 +0000

    Golfing

--- CBConcrete/CBConcrete/Lib/Fin.lean
+++ CBConcrete/CBConcrete/Lib/Fin.lean
@@ -1,10 +1,3 @@
-import Batteries.Data.Fin.Lemmas
-
-@[grind =]
-theorem Option.getD_eq_dite {α} {o : Option α} {a : α} :
-    o.getD a = if h : o.isSome then o.get h else a := by grind [cases Option]
-
-
 namespace Fin
 
 variable {n : Nat}
@@ -12,46 +5,25 @@ variable {n : Nat}
 @[simp, grind =]
 theorem coe_xor {i j : Fin n} : (i ^^^ j).val = (i.val ^^^ j.val) % n := rfl
 
-/-
-@[grind =]
-theorem coe_succAbove {i : Fin n} {p : Fin (n + 1)} :
-    (p.succAbove i : ℕ) = if p.val ≤ i.val then i.val + 1 else i.val := by
-  grind [succAbove, val_castSucc]
+theorem surj_of_inj (f : Fin n → Fin n) (hf : ∀ x y, f x = f y → x = y) : ∀ y, ∃ x, f x = y := by
+  induction n with | zero => exact (·.elim0) | succ n IH
+  have H (g : Fin (n + 1) → Fin (n + 1)) (hg' : g (last n) = last n)
+    (hg' : ∀ (x y : Fin (n + 1)), g x = g y → x = y) : ∀ y, ∃ x, g x = y := lastCases (by grind)
+    (fun i => by specialize IH (⟨g ⟨·, by grind⟩, by grind⟩) (by grind) i; grind)
+  intro y; specialize H (fun i => if i = last n then last n else
+    if f i = last n then f (last n) else f i) (by grind) (by grind)
+    (if y = f (last n) then last n else if y = last n then f (last n) else y); grind
 
-@[grind =]
-theorem coe_predAbove {p : Fin n} {i : Fin (n + 1)} :
-    (p.predAbove i : ℕ) = if p.val < i.val then i.val - 1 else i.val := by
-  grind [predAbove, val_castSucc, coe_castPred]
--/
+theorem inj_of_surj (g : Fin n → Fin n) (hg : ∀ y, ∃ x, g x = y) : ∀ x y, g x = g y → x = y := by
+  have hf x y (hxy : (hg ·|>.choose) x = (hg ·|>.choose) y) : x = y :=
+    (Exists.choose_spec <| hg _).symm.trans <| (congrArg g hxy).trans <| Exists.choose_spec <| hg _
+  intro x y hxy; have := And.intro (surj_of_inj _ hf x) (surj_of_inj _ hf y); grind
 
-theorem surj_of_inj (f : Fin n → Fin n) (hf : ∀ x y, f x = f y → x = y) (y : Fin n) :
-     ∃ x, f x = y := by
-  induction n with | zero | succ n IH
-  · grind [cases Fin]
-  · have H (g : Fin (n + 1) → Fin (n + 1)) (hg' : g (last n) = last n)
-      (hg' : ∀ (x y : Fin (n + 1)), g x = g y → x = y) (y : Fin (n + 1)) : ∃ x, g x = y :=
-      y.lastCases (by grind)
-        fun i => have := IH (fun i => ⟨g ⟨i, by grind⟩, by grind⟩) (by grind) i; by grind
-    specialize H (fun i => if i = last n then last n else
-      if f i = last n then f (last n) else f i) (by grind) (by grind)
-      (if y = f (last n) then last n else if y = last n then f (last n) else y)
-    grind
+theorem surjective_of_injective : (f : Fin n → Fin n) → f.Injective → f.Surjective := surj_of_inj
 
-theorem inj_of_surj (g : Fin n → Fin n) (hg : ∀ y, ∃ x, g x = y) (x y : Fin n) :
-    g x = g y → x = y := by
-  have hf : ∀ x y, (hg ·|>.choose) x = (hg ·|>.choose) y → x = y :=
-    fun x y hxy => have := congrArg g hxy; by grind
-  have Hx := surj_of_inj _ hf x; have Hy := surj_of_inj _ hf y; grind
+theorem injective_of_surjective : (f : Fin n → Fin n) → f.Surjective → f.Injective := inj_of_surj
 
-open Function in
-theorem surjective_of_injective (f : Fin n → Fin n) : Injective f → Surjective f := surj_of_inj f
-
-open Function in
-theorem injective_of_surjective (f : Fin n → Fin n) : Surjective f → Injective f := inj_of_surj f
-
-open Function in
-theorem injective_iff_surjective (f : Fin n → Fin n) : Injective f ↔ Surjective f :=
+theorem injective_iff_surjective (f : Fin n → Fin n) : f.Injective ↔ f.Surjective :=
   ⟨surjective_of_injective f, injective_of_surjective f⟩
-
 
 end Fin
